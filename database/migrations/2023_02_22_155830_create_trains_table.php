0d4c37a322128144ee6e8010b4e7dd8f888f917f
N1colas7 <nicolasnd9897@gmail.com>
2023-02-22 17:10:20 +0100

    first migrate

--- database/migrations/2023_02_22_155830_create_trains_table.php
+++ database/migrations/2023_02_22_155830_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('azienda', 100);
+            $table->string('stazione_partenza', 100);
+            $table->string('stazione_arrivo', 100);
+            $table->time('orario_partenza');
+            $table->time('orario_arrivo');
+            $table->string('codice_treno', 7);
+            $table->smallInteger('num_carrozze');
+            $table->boolean('in_orario')->default(1);
+            $table->boolean('cancellato')->default(0);
             $table->timestamps();
         });
     }
