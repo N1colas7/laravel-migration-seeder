27fb6140b27220e6a0a695a1d61b3b15496c6b5b
N1colas7 <nicolasnd9897@gmail.com>
2023-02-22 17:15:47 +0100

    add to seeder faker and models

---
+++ database/seeders/SeederTrainTable.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\Train;
+use Faker\Generator as Faker;
+
+class SeederTrainTable extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        //
+    }
+}
